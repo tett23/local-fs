8a498f87bac31746c30adc9c40f1f9b5154fdc1a
tett23 <tett23@gmail.com>
2020-04-11 12:50:43 +0900

    Implement keys and clear method

--- packages/simple-local-kvs/__tests__/index.test.ts
+++ packages/simple-local-kvs/__tests__/index.test.ts
@@ -1,69 +1,81 @@
-import localKVS from '../src/index';
+import localKVS, { KVS } from '../src/index';
 import 'fake-indexeddb';
 
-describe('put', () => {
-  it('puts any value', async () => {
-    const kvs = await localKVS(indexedDB);
+async function kvsInstance() {
+  const kvs = await localKVS(indexedDB);
+  if (kvs instanceof Error) {
+    throw kvs;
+  }
+  const clearResult = await kvs.clear();
+  if (clearResult instanceof Error) {
+    throw clearResult;
+  }
 
-    if (kvs instanceof Error) {
-      expect(kvs).not.toBeInstanceOf(Error);
-      return;
-    }
+  return kvs;
+}
 
-    const result = await kvs.put('foo', 'bar');
-    expect(result).not.toBeInstanceOf(Error);
-    expect(result).toBe(true);
-  });
-});
-
-describe('get', () => {
+describe('KVS', () => {
+  let kvs: KVS;
   beforeEach(async () => {
-    const kvs = await localKVS(indexedDB);
-    if (kvs instanceof Error) {
-      expect(kvs).not.toBeInstanceOf(Error);
-      return;
-    }
-
-    const result = await kvs.put('foo', 'bar');
-    expect(result).not.toBeInstanceOf(Error);
-    expect(result).toBe(true);
+    kvs = await kvsInstance();
   });
 
-  it('get any value', async () => {
-    const kvs = await localKVS(indexedDB);
-
-    if (kvs instanceof Error) {
-      expect(kvs).not.toBeInstanceOf(Error);
-      return;
-    }
-
-    const value = await kvs.get('foo');
-    expect(value).toBe('bar');
-  });
-});
-
-describe('delete', () => {
-  beforeEach(async () => {
-    const kvs = await localKVS(indexedDB);
-    if (kvs instanceof Error) {
-      expect(kvs).not.toBeInstanceOf(Error);
-      return;
-    }
-
-    await kvs.put('key1', 'foo');
-    await kvs.put('key2', 'bar');
+  describe('put', () => {
+    it('puts any value', async () => {
+      const result = await kvs.put('foo', 'bar');
+      expect(result).not.toBeInstanceOf(Error);
+      expect(result).toBe(true);
+    });
   });
 
-  it('delete any value', async () => {
-    const kvs = await localKVS(indexedDB);
+  describe('get', () => {
+    beforeEach(async () => {
+      const result = await kvs.put('foo', 'bar');
+      expect(result).not.toBeInstanceOf(Error);
+      expect(result).toBe(true);
+    });
 
-    if (kvs instanceof Error) {
-      expect(kvs).not.toBeInstanceOf(Error);
-      return;
-    }
+    it('get any value', async () => {
+      const value = await kvs.get('foo');
+      expect(value).toBe('bar');
+    });
+  });
 
-    expect(await kvs.delete('key1')).toBe(true);
-    expect(await kvs.get('key1')).toBe(undefined);
-    expect(await kvs.delete('does_not_exist')).toBe(true);
+  describe('delete', () => {
+    beforeEach(async () => {
+      await kvs.put('key1', 'foo');
+      await kvs.put('key2', 'bar');
+    });
+
+    it('delete a value', async () => {
+      expect(await kvs.delete('key1')).toBe(true);
+      expect(await kvs.get('key1')).toBe(undefined);
+      expect(await kvs.keys()).toEqual(['key2']);
+      expect(await kvs.delete('does_not_exist')).toBe(true);
+    });
+  });
+
+  describe('keys', () => {
+    beforeEach(async () => {
+      await kvs.put('key1', 'foo');
+      await kvs.put('key2', 'bar');
+    });
+
+    it('delete any value', async () => {
+      expect(await kvs.keys()).toEqual(['key1', 'key2']);
+    });
+  });
+
+  describe('clear', () => {
+    beforeEach(async () => {
+      await kvs.put('key1', 'foo');
+      await kvs.put('key2', 'bar');
+    });
+
+    it('clear all keys', async () => {
+      expect(await kvs.keys()).toEqual(['key1', 'key2']);
+      expect(await kvs.clear()).toEqual(true);
+      expect(await kvs.keys()).toEqual([]);
+    });
   });
 });
--- packages/simple-local-kvs/src/index.ts
+++ packages/simple-local-kvs/src/index.ts
@@ -1,4 +1,4 @@
-const DBNaame = 'SimpleLocalKVS';
+const DBName = 'SimpleLocalKVS';
 const StoreName = 'LocalKVS';
 const Version = 1;
 
@@ -8,23 +8,34 @@ export type KVS = {
   get: <T>(key: string, defaultValue?: T) => Promise<UndefinedOr<T> | Error>;
   put: <T>(key: string, value: T) => Promise<boolean | Error>;
   delete: (key: string) => Promise<boolean | Error>;
+  keys: () => Promise<string[] | Error>;
+  clear: () => Promise<boolean | Error>;
 };
 
-export default async function(indexedDB: IDBFactory): Promise<KVS | Error> {
-  const kvs = await getKVS(indexedDB);
+export default async function (
+  indexedDB: IDBFactory,
+  storeName: string = StoreName,
+): Promise<KVS | Error> {
+  const kvs = await getKVS(indexedDB, storeName);
   if (kvs instanceof Error) {
     return kvs;
   }
 
   return {
-    get: <T>(key: string, defaultValue?: T) => get(kvs, key, defaultValue),
-    put: <T>(key: string, value: T) => put(kvs, key, value),
-    delete: (key: string) => remove(kvs, key),
+    get: <T>(key: string, defaultValue?: T) =>
+      get(kvs, storeName, key, defaultValue),
+    put: <T>(key: string, value: T) => put(kvs, storeName, key, value),
+    delete: (key: string) => remove(kvs, storeName, key),
+    clear: () => clear(kvs, storeName),
+    keys: () => keys(kvs, storeName),
   };
 }
 
-async function getKVS(indexedDB: IDBFactory): Promise<IDBDatabase | Error> {
-  const openRequest = indexedDB.open(DBNaame, Version);
+async function getKVS(
+  indexedDB: IDBFactory,
+  storeName: string,
+): Promise<IDBDatabase | Error> {
+  const openRequest = indexedDB.open(DBName, Version);
 
   return new Promise<IDBDatabase | Error>((resolve) => {
     openRequest.onerror = () => {
@@ -32,7 +43,7 @@ async function getKVS(indexedDB: IDBFactory): Promise<IDBDatabase | Error> {
     };
 
     openRequest.onupgradeneeded = () => {
-      const objectStore = openRequest.result.createObjectStore(StoreName);
+      const objectStore = openRequest.result.createObjectStore(storeName);
 
       objectStore.transaction.oncomplete = () => {
         resolve(openRequest.result);
@@ -47,12 +58,13 @@ async function getKVS(indexedDB: IDBFactory): Promise<IDBDatabase | Error> {
 
 async function get<T>(
   db: IDBDatabase,
+  storeName: string,
   key: string,
   defaultValue?: T,
 ): Promise<UndefinedOr<T> | Error> {
   const req: IDBRequest<T> = db
-    .transaction(StoreName, 'readonly')
-    .objectStore(StoreName)
+    .transaction(storeName, 'readonly')
+    .objectStore(storeName)
     .get(key);
 
   return new Promise<UndefinedOr<T> | Error>((resolve) => {
@@ -73,12 +85,13 @@ async function get<T>(
 
 async function put<T>(
   db: IDBDatabase,
+  storeName: string,
   key: string,
   value: T,
 ): Promise<boolean | Error> {
   const req = db
-    .transaction(StoreName, 'readwrite')
-    .objectStore(StoreName)
+    .transaction(storeName, 'readwrite')
+    .objectStore(storeName)
     .put(value, key);
 
   return new Promise<boolean | Error>((resolve) => {
@@ -92,10 +105,14 @@ async function put<T>(
   }).catch((err: Error) => err);
 }
 
-async function remove(db: IDBDatabase, key: string): Promise<boolean | Error> {
+async function remove(
+  db: IDBDatabase,
+  storeName: string,
+  key: string,
+): Promise<boolean | Error> {
   const req = db
-    .transaction(StoreName, 'readwrite')
-    .objectStore(StoreName)
+    .transaction(storeName, 'readwrite')
+    .objectStore(storeName)
     .delete(key);
 
   const ret = new Promise<boolean | Error>((resolve) => {
@@ -103,7 +120,60 @@ async function remove(db: IDBDatabase, key: string): Promise<boolean | Error> {
       resolve(req.error || new Error('Unexpected error.'));
     };
 
-    req.onsuccess = (a) => {
+    req.onsuccess = () => {
+      resolve(true);
+    };
+  }).catch((err: Error) => err);
+
+  return ret;
+}
+
+async function keys(
+  db: IDBDatabase,
+  storeName: string,
+): Promise<string[] | Error> {
+  const req = db
+    .transaction(storeName, 'readwrite')
+    .objectStore(storeName)
+    .openKeyCursor();
+
+  let ret = [];
+  await new Promise<boolean | Error>((resolve) => {
+    req.onerror = () => {
+      resolve(req.error || new Error('Unexpected error.'));
+    };
+
+    req.onsuccess = (e: any) => {
+      const cursor = e.target.result;
+      if (cursor == null) {
+        resolve(true);
+        return;
+      }
+
+      ret.push(cursor.key);
+
+      cursor.continue();
+    };
+  }).catch((err: Error) => err);
+
+  return ret;
+}
+
+async function clear(
+  db: IDBDatabase,
+  storeName: string,
+): Promise<boolean | Error> {
+  const req = db
+    .transaction(storeName, 'readwrite')
+    .objectStore(storeName)
+    .clear();
+
+  const ret = new Promise<boolean | Error>((resolve) => {
+    req.onerror = () => {
+      resolve(req.error || new Error('Unexpected error.'));
+    };
+
+    req.onsuccess = () => {
       resolve(true);
     };
   }).catch((err: Error) => err);
